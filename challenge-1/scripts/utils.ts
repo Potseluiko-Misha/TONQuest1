633a6b22d79dd2109f21a05762538d65c5257df0
Misha <vechnuignor@mail.ru>
2024-09-02 22:11:37 +0300

    end

--- challenge-1/scripts/utils.ts
+++ challenge-1/scripts/utils.ts
@@ -2,7 +2,7 @@ import { TonClient } from 'ton';
 import { Address } from 'ton-core';
 
 export const toncenter = new TonClient({
-	endpoint: 'https://testnet.toncenter.com/api/v2/jsonRPC',
+	endpoint: 'https://toncenter.com/api/v2/jsonRPC',
 });
 
 export const nftCollectionAddress = Address.parse('EQDf6HCOggN_ZGL6YsYleN6mDiclQ_NJOMY-x8G5cTRDOBW4');
